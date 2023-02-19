c72133ddcb36acf138583ee8c449c507f7e86951
Ben Bowen <bcbowen@gmail.com>
2023-02-18 21:50:21 -0500

    Fizzbuzz

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,21 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+    for (let i = 1; i <= n; i++) {
+        let val = '';
+        if (i % 3 === 0) {
+          val = 'fizz'; 
+        } 
+        if (i % 5 === 0) {
+          val += 'buzz'; 
+        } 
+          
+        val = val || i;
+        
+        console.log(val);
+      }
+      
+}
 
 module.exports = fizzBuzz;
