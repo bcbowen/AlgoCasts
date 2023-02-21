37dc8911cbd12fce583f482ebb1d4569ea18f879
Ben Bowen <bcbowen@gmail.com>
2023-02-20 23:13:23 -0500

    added steps exercise

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,44 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+    for (let r = 0; r < n; r++) {
+        let line = ''; 
+        for (let c = 0; c < n; c++)
+        {
+            if (c <= r){
+                line += '#';
+            } else {
+                line += ' ';
+            }
+
+        }
+        console.log(line);
+    }
+    
+}
 
 module.exports = steps;
+
+
+/*
+function steps(n) {
+    let step = 1;
+    let line = '';
+    for(let i = 0; i < n; i++){
+        line = '';
+        for (let j = 0; j < step; j++) {
+            line += "#";
+        }
+        for (let k = i + 1; k < n; k++)
+        {
+            line += ' ';
+        }
+        
+        console.log(line);
+        step++;
+        
+    } 
+}
+
+*/
