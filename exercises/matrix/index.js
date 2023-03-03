5491aec5e0b8f40bf2917d7c641cd7c2491b29d7
Ben Bowen <bcbowen@gmail.com>
2023-03-03 16:53:49 -0500

    Working on matrix exercise... not working yet

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,112 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  let spiral = [];
+  let row = [];
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      row.push(0);
+    }
+    spiral.push(row);
+  }
+
+  let minRow = 0;
+  let maxRow = n - 1;
+  let minCol = 0;
+  let maxCol = n - 1;
+  let x = 0;
+  let y = 0;
+  let i = 1;
+  const maxI = n * n;
+  while (i <= maxI) {
+    while (x++ <= maxCol) {
+      spiral[y][x] = i;
+    }
+    i++;
+    minRow++;
+
+    while (y++ <= maxRow) {
+      spiral[y][x] = i;
+    }
+    i++;
+    maxCol--;
+
+    while (x-- >= minCol) {
+      spiral[y][x] = i;
+    }
+    i++;
+    maxRow--;
+
+    while (y-- >= minRow) {
+      spiral[y][x] = i;
+    }
+    i++;
+    minCol++;
+  }
+
+  return spiral;
+}
 
 module.exports = matrix;
+
+/*
+function matrix(n) {
+  const spiral = [];
+  let row = [];
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      row.push(0);
+    }
+    spiral.push(row);
+  }
+
+  let x = 0;
+  let y = 0;
+  const right = 0;
+  const down = 1;
+  const left = 2;
+  const up = 3;
+  let direction = right;
+  const maxValue = n * n;
+  for (let i = 1; maxValue; i++) {
+    spiral[y][x] = i;
+    switch (direction) {
+      case right:
+        if (x < n - 1 && spiral[y][x + 1] == 0) {
+          x++;
+        } else {
+          direction = down;
+          y++;
+        }
+        break;
+      case down:
+        if (y < n - 1 && spiral[y + 1][x] == 0) {
+          y++;
+        } else {
+          direction = left;
+          x--;
+        }
+        break;
+      case left:
+        if (x > 0 && spiral[y][x - 1] == 0) {
+          x--;
+        } else {
+          direction = up;
+          y--;
+        }
+        break;
+      case up:
+        if (y > 0 && spiral[y - 1][x] == 0) {
+          y--;
+        } else {
+          direction = right;
+          x++;
+        }
+        break;
+    }
+  }
+
+  return spiral;
+}
+*/
