91dd69cf10a403d91bb8ec526056e6c035024339
Ben Bowen <bcbowen@gmail.com>
2023-03-04 17:06:19 -0500

    Add memoized recursive fib

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -7,7 +7,29 @@
 // forms the first ten entries of the fibonacci series.
 // Example:
 //   fib(4) === 3
+function memoize(fn) {
+  const cache = {};
+  return function (...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+    const result = fn.apply(this, args);
+    cache[args] = result;
+    return result;
+  };
+}
 
+function fib(n) {
+  if (n < 2) return n;
+
+  return fib(n - 1) + fib(n - 2);
+}
+
+fib = memoize(fib);
+
+module.exports = fib;
+
+/* iterative O(n): 
 function fib(n) {
   let nums = [0, 1, 1];
 
@@ -19,4 +41,4 @@ function fib(n) {
   return nums[n];
 }
 
-module.exports = fib;
+*/
