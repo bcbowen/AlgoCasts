348447f504fad73ae2fa6e3e7f2e960d03382a65
Ben Bowen <bcbowen@gmail.com>
2023-03-12 23:43:27 -0400

    Validate bst exercise

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -5,6 +5,30 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+function validate(node, min = null, max = null) {
+    let isValid = true;
+
+    if (min && node.data < min) isValid = false;
+    if (max && node.data > max) isValid = false;
+
+    if (isValid && node.left){
+        if (node.left.data > node.data) {
+            isValid = false;
+        } else {
+            isValid = validate(node.left, min, node.data);
+        }
+    
+    }
+    
+    if (isValid && node.right) {
+        if (node.right.data < node.data) {
+            isValid = false;
+        } else {
+            isValid = validate(node.right, min = node.data, max);
+        }
+    }
+
+    return isValid;
+}
 
 module.exports = validate;
